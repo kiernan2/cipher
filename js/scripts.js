e62d37218604424a69699da996bdbe55539eb518
Kiernan2 <kiernan1994@gmail.com>
2021-06-17 15:30:40 -0700

    Bootstrap should work now

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 function grab(x) {
   return  x.charAt(0).toUpperCase() + x.charAt(x.length-1).toUpperCase();
-}
+};
 
 function reverse(x) {
   return x.charAt(x.length-1) + x.charAt(0);
-}
+};
 
 function encrypt(x) {
-  return divide(x) + x + reverse(grab(x))
-}
+  return divide(x) + x + reverse(grab(x));
+};
 
 function divide(x){
-  return x.charAt(x.length/2)
-}
+  return x.charAt(x.length/2);
+};
 
 const words = prompt("Enter words");
 const result = encrypt(words);
-alert(result);
+
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert(result)
+  })
+  jQuery("h2").click(function() {
+    alert(words)
+  })
+});
